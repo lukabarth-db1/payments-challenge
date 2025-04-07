a7f8204571af6695b937f7f57dedbde23c24fcea
Luka Barth <luka.costa@db1.com.br>
2025-04-07 17:25:51 -0300

    Revert "refactor: refactored controller"
    This reverts commit 89e84b5551f96b01b2f989fdfc5abbc08b266f6e.

--- src/API/Http/Controller/PaymentsController.php
+++ src/API/Http/Controller/PaymentsController.php
@@ -6,8 +6,8 @@ namespace App\API\Http\Controller;
 
 use Phractico\Core\Facades\Database;
 use Phractico\Core\Facades\DatabaseOperation;
+use Phractico\Core\Infrastructure\Database\Query\Grammar\Comparison;
 use Phractico\Core\Infrastructure\Database\Query\Statement;
-use Phractico\Core\Infrastructure\Database\Result;
 use Phractico\Core\Infrastructure\Http\Controller;
 use Phractico\Core\Infrastructure\Http\Request\RequestHandler;
 use Phractico\Core\Infrastructure\Http\Request\Route;
@@ -17,8 +17,6 @@ use Phractico\Core\Infrastructure\Http\Response\JsonResponse;
 
 class PaymentsController implements Controller
 {
-    private array $requestBody = [];
-
     public function routes(): RouteCollection
     {
         $routes = RouteCollection::for($this);
@@ -26,92 +24,63 @@ class PaymentsController implements Controller
         return $routes;
     }
 
-    public function getRequestBody(): void
+    public function createPayment(): Response
     {
         // Pegando o corpo da requisição
         $request = RequestHandler::getIncomingRequest();
-        $contents = $request->getBody()->getContents();
-        $this->requestBody = json_decode($contents, true);
-    }
+        $requestBody = $request->getBody()->getContents();
+        $decodedRequestBody = json_decode($requestBody, true);
 
-    public function mappingValuesCustomer(): array
-    {
         // Mapeando valores do customer para a tabela customer do banco de dados
-        return [
-            'name' => $this->requestBody['customer']['name'],
-            'email' => $this->requestBody['customer']['email'],
-            'document' => $this->requestBody['customer']['document'],
+        $customerData = [
+            'name' => $decodedRequestBody['customer']['name'],
+            'email' => $decodedRequestBody['customer']['email'],
+            'document' => $decodedRequestBody['customer']['document'],
         ];
-    }
 
-    public function prepareStatementCustomer(): void
-    {
         // Preparando statement para inserção dos valores na tabela customer do banco de dados
         $statement = DatabaseOperation::table('customers')
             ->insert()
-            ->data($this->mappingValuesCustomer())
+            ->data($customerData)
             ->build();
         Database::execute($statement);
-    }
 
-    public function getCustomerId(): int
-    {
-        // Recuperar valor do id na tabela customer do banco de dados
+        // Recuperar valor do customer do banco de dados
         $statement = new Statement(
             "SELECT * FROM customers ORDER BY id DESC LIMIT 1"
         );
         $statement->returningResults();
-        $result = Database::execute($statement);
+        $customerSelectResult = Database::execute($statement);
 
-        return $result->getRows()[0]['id'];
-    }
+        // Acessando o id do customer
+        $customer_id = $customerSelectResult->getRows()[0]['id'];
 
-    public function mappingValuesPayments(): array
-    {
         // Mapeando valores para tabela de pagamentos
-        return [
-            'amount' => $this->requestBody['payment']['amount'],
-            'type' => $this->requestBody['payment']['type'],
-            'country' => $this->requestBody['payment']['country'],
-            'customer_id' => $this->getCustomerId(),
+        $paymentData = [
+            'amount' => $decodedRequestBody['payment']['amount'],
+            'type' => $decodedRequestBody['payment']['type'],
+            'country' => $decodedRequestBody['payment']['country'],
+            'customer_id' => $customer_id,
         ];
-    }
 
-    public function prepareStatementPayment(): void
-    {
         // Preparando statement para inserção dos valores na tabela payment do banco de dados
         $statement = DatabaseOperation::table('payments')
             ->insert()
-            ->data($this->mappingValuesPayments())
+            ->data($paymentData)
             ->build();
         Database::execute($statement);
-    }
 
-    public function getPaymentValue(): Result
-    {
         // Recuperando valor payment do banco de dados
         $statement = new Statement(
             "SELECT * FROM payments ORDER BY id DESC LIMIT 1"
         );
         $statement->returningResults();
+        $paymentSelectResult = Database::execute($statement);
 
-        return Database::execute($statement);
-    }
-
-    public function responseBodyInJson(): Response
-    {
         // Preparando a resposta da requisição
         $body = [
-            $this->getPaymentValue()->getRows()
+            $paymentSelectResult->getRows()
         ];
         return new JsonResponse(201, $body);
-    }
-
-    public function createPayment(): Response
-    {
-        $this->getRequestBody();
-        $this->prepareStatementCustomer();
-        $this->prepareStatementPayment();
-        return $this->responseBodyInJson();
     }
 }
