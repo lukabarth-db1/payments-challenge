b166412b29c2dbf2949f1daf80bbda394fbeb9ac
Luka Barth <luka.costa@db1.com.br>
2025-04-14 17:41:35 -0300

    refactor: refactored controller and injected dependecies

--- src/API/Http/Controller/PaymentsController.php
+++ src/API/Http/Controller/PaymentsController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\API\Http\Controller;
 
+use App\Gateway\Contracts\PaymentGatewayInterface;
+use App\Gateway\PagueFacil;
+use App\Service\Payments\CancelPaymentService;
+use App\Service\Payments\ConfirmPaymentService;
+use App\Service\Payments\CreatePaymentService;
+use App\Service\Payments\RefundPaymentService;
+use App\Service\Providers\ProviderLogService;
+use DomainException;
 use GuzzleHttp\Psr7\Request;
-use Phractico\Core\Facades\Database;
-use Phractico\Core\Facades\DatabaseOperation;
-use Phractico\Core\Infrastructure\Database\Query\Grammar\Comparison;
-use Phractico\Core\Infrastructure\Database\Query\Statement;
 use Phractico\Core\Infrastructure\Http\Controller;
 use Phractico\Core\Infrastructure\Http\Request\RequestHandler;
 use Phractico\Core\Infrastructure\Http\Request\Route;
@@ -18,6 +22,11 @@ use Phractico\Core\Infrastructure\Http\Response\JsonResponse;
 
 class PaymentsController implements Controller
 {
+    public function __construct(
+        private PaymentGatewayInterface $paymentGateway = new PagueFacil(),
+        private ProviderLogService $providerLogService = new ProviderLogService()
+    ) {}
+
     private array $requestBody = [];
 
     public function routes(): RouteCollection
@@ -35,12 +44,22 @@ class PaymentsController implements Controller
         $request = RequestHandler::getIncomingRequest();
         $this->requestBody = $this->decodeRequestBody($request);
 
-        $this->persistPayment();
+        $operationType = 'create';
 
-        $payment = $this->retrieveLastInsertedPayment();
+        $gatewayResponse = $this->paymentGateway->create($this->requestBody);
+
+        $createPaymentService = new CreatePaymentService($this->requestBody);
+        $payment = $createPaymentService->execute();
+
+        $this->providerLogService->log(
+            $gatewayResponse['gateway'],
+            $operationType,
+            $payment['id']
+        );
 
         return new JsonResponse(201, [
-            'payment' => $payment
+            'payment' => $payment,
+            'provider_logs' => "Payment successfuly created by {$gatewayResponse['gateway']}",
         ]);
     }
 
@@ -50,21 +69,19 @@ class PaymentsController implements Controller
         $this->requestBody = $this->decodeRequestBody($request);
 
         $paymentId = $this->requestBody['payment']['id'];
-        $statusConfirmed = 'confirmed';
 
-        $currentStatus = $this->getPaymentStatus($paymentId);
+        try {
+            $service = new ConfirmPaymentService($this->requestBody);
+            $service->execute($paymentId);
 
-        if ($currentStatus !== 'pending') {
+            return new JsonResponse(200, [
+                'message' => 'payment confirmed'
+            ]);
+        } catch (DomainException $e) {
             return new JsonResponse(400, [
-                'error' => "payment id {$paymentId} cannot be confirm"
+                'error' => $e->getMessage()
             ]);
         }
-
-        $this->updatePaymentStatus($paymentId, $statusConfirmed);
-
-        return new JsonResponse(200, [
-            'message' => 'payment confirmed'
-        ]);
     }
 
     public function cancelPayment(): Response
@@ -73,21 +90,18 @@ class PaymentsController implements Controller
         $this->requestBody = $this->decodeRequestBody($request);
 
         $paymentId = $this->requestBody['payment']['id'];
-        $statusCanceled = 'canceled';
 
-        $currentStatus = $this->getPaymentStatus($paymentId);
-
-        if ($currentStatus !== 'pending') {
+        try {
+            $service = new CancelPaymentService($this->requestBody);
+            $service->execute($paymentId);
+            return new JsonResponse(200, [
+                'message' => 'payment canceled'
+            ]);
+        } catch (DomainException $e) {
             return new JsonResponse(400, [
-                'error' => "payment id {$paymentId} cannot be cancel"
+                'error' => $e->getMessage()
             ]);
         }
-
-        $this->updatePaymentStatus($paymentId, $statusCanceled);
-
-        return new JsonResponse(200, [
-            'payment' => "payment id '{$paymentId}' is canceled"
-        ]);
     }
 
     public function refundPayment(): Response
@@ -96,119 +110,23 @@ class PaymentsController implements Controller
         $this->requestBody = $this->decodeRequestBody($request);
 
         $paymentId = $this->requestBody['payment']['id'];
-        $statusRefund = 'refund';
 
-        $currentStatus = $this->getPaymentStatus($paymentId);
-
-        if ($currentStatus !== 'confirmed') {
+        try {
+            $service = new RefundPaymentService($this->requestBody);
+            $service->execute($paymentId);
+            return new JsonResponse(200, [
+                'message' => 'payment refunded'
+            ]);
+        } catch (DomainException $e) {
             return new JsonResponse(400, [
-                'error' => "payment id {$paymentId} cannot be refunded"
+                'error' => $e->getMessage()
             ]);
         }
-
-        $this->updatePaymentStatus($paymentId, $statusRefund);
-
-        return new JsonResponse(200, [
-            'message' => "payment id {$paymentId} refund"
-        ]);
-    }
-
-    private function getPaymentStatus(int $paymentId): string
-    {
-        $statement = new Statement("SELECT status FROM payments WHERE id = {$paymentId}");
-        $statement->returningResults();
-
-        $result = Database::execute($statement)->getRows()[0]['status'];
-
-        return $result;
-    }
-
-    private function updatePaymentStatus(int $id, string $status): void
-    {
-        $statement = DatabaseOperation::table('payments')
-            ->update()
-            ->data(['status' => $status])
-            ->where('id', Comparison::EQUAL, $id)
-            ->build();
-        Database::execute($statement);
     }
 
     private function decodeRequestBody(Request $request): array
     {
         $contents = $request->getBody()->getContents();
         return json_decode($contents, true);
-    }
-
-    private function persistCustomer(): void
-    {
-        $statement = DatabaseOperation::table('customers')
-            ->insert()
-            ->data($this->mappingValuesCustomers())
-            ->build();
-
-        Database::execute($statement);
-    }
-
-    private function mappingValuesCustomers(): array
-    {
-        return [
-            'name' => $this->requestBody['customer']['name'],
-            'email' => $this->requestBody['customer']['email'],
-            'document' => $this->requestBody['customer']['document'],
-        ];
-    }
-
-    private function persistPayment(): void
-    {
-        $statement = DatabaseOperation::table('payments')
-            ->insert()
-            ->data($this->mappingValuesPayments())
-            ->build();
-
-        Database::execute($statement);
-    }
-
-    private function mappingValuesPayments(): array
-    {
-        return [
-            'amount' => $this->requestBody['payment']['amount'],
-            'type' => $this->requestBody['payment']['type'],
-            'country' => $this->requestBody['payment']['country'],
-            'status' => $this->requestBody['payment']['status'],
-            'customer_id' => $this->getOrCreateCustomerId(),
-        ];
-    }
-
-    private function getOrCreateCustomerId(): int
-    {
-        $email = $this->requestBody['customer']['email'];
-
-        $statement = new Statement("SELECT id FROM customers WHERE email = '{$email}'");
-        $statement->returningResults();
-
-        $result = Database::execute($statement)->getRows();
-
-        if (!empty($result)) {
-            return $result[0]['id'];
-        }
-
-        $this->persistCustomer();
-        return $this->getLastCustomerId();
-    }
-
-    private function getLastCustomerId(): int
-    {
-        $statement = new Statement("SELECT id FROM customers ORDER BY id DESC LIMIT 1");
-        $statement->returningResults();
-
-        return Database::execute($statement)->getRows()[0]['id'];
-    }
-
-    private function retrieveLastInsertedPayment(): array
-    {
-        $statement = new Statement("SELECT * FROM payments ORDER BY id DESC LIMIT 1");
-        $statement->returningResults();
-
-        return Database::execute($statement)->getRows()[0];
     }
 }
