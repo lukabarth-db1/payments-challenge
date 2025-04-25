9b68d3691dced89c306d3bc9633a9e19ad0ccb0e
Luka Barth <luka.costa@db1.com.br>
2025-04-25 07:37:45 -0300

    refactor: change argument type

---
+++ src/Gateway/Contracts/PaymentGateway.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Gateway\Contracts;
+
+use App\Gateway\Contracts\Dto\GatewayPaymentInfo;
+use Gateway\Contracts\Dto\GatewayResponse;
+
+interface PaymentGateway
+{
+    public function create(GatewayPaymentInfo $data): GatewayResponse;
+    // public function confirm(int $paymentId): void;
+    // public function cancel(int $paymentId): void;
+    // public function refund(int $paymentId): void;
+}
--- src/Gateway/PagueFacil.php
+++ src/Gateway/PagueFacil.php
@@ -4,22 +4,24 @@ declare(strict_types=1);
 
 namespace App\Gateway;
 
-use App\Gateway\Contracts\PaymentGatewayInterface;
+use App\Gateway\Contracts\Dto\GatewayPaymentInfo;
+use App\Gateway\Contracts\PaymentGateway;
 use Exception;
+use Gateway\Contracts\Dto\GatewayResponse;
 
-class PagueFacil implements PaymentGatewayInterface
+class PagueFacil implements PaymentGateway
 {
-    public function create(array $data): array
+    public function create(GatewayPaymentInfo $data): GatewayResponse
     {
-        $country = $data['payment']['country'];
+        $country = $data->country;
 
         if ($country !== 'BR') {
             throw new Exception("PagueFacil only accepts payments from Brazil");
         }
 
-        return [
-            'status' => 'pending',
-            'gateway' => 'PagueFacil'
-        ];
+        return new GatewayResponse(
+            'pending',
+            'PagueFacil',
+        );
     }
 }
