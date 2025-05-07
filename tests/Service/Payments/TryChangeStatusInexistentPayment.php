3de178c41bb8c953733017e861c8ecd39ab02bad
Luka Barth <luka.costa@db1.com.br>
2025-05-07 10:19:19 -0300

    test: add test to verify a valid payment

---
+++ tests/Service/Payments/TryChangeStatusInexistentPayment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Service\Payments;
+
+use App\Database\Connection\SQLiteAdapter;
+use App\Exceptions\PaymentStatusException;
+use App\Gateway\GatewayOperation;
+use App\Service\Payments\ConfirmPaymentService;
+use App\Service\Payments\Dto\ProviderStatusInfo;
+use App\Service\Payments\PaymentStatusService;
+use App\Service\Providers\ProviderLogService;
+use PHPUnit\Framework\TestCase;
+use Phractico\Core\Infrastructure\Database\DatabaseConnection;
+
+class TryChangeStatusInexistentPayment extends TestCase
+{
+    /**
+     * @before
+     */
+    public function init(): void
+    {
+        $connection = new SQLiteAdapter(__DIR__ . '/../../../database-test.sqlite');
+        DatabaseConnection::setConnection($connection);
+    }
+
+    public function testExecute_ShouldThrowExceptionWhenPaymentDoesNotExist(): void
+    {
+        $this->expectException(PaymentStatusException::class);
+        $this->expectExceptionMessage("Invalid ID");
+
+        $paymentStatusService = new PaymentStatusService();
+
+        $providerStatus = new ProviderStatusInfo(
+            provider: 'PagueFacil',
+            operation: GatewayOperation::CREATE->value,
+            paymentId: 777,
+        );
+
+        $providerLog = new ProviderLogService();
+        $confirmPaymentService = new ConfirmPaymentService($paymentStatusService, $providerLog);
+
+        $confirmPaymentService->execute($providerStatus);
+    }
+}
